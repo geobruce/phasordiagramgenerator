7c569ecfff9dcefadaa8e101a7bcf88c54d00607
geobruce <brucehelsen@hotmail.com>
2019-11-09 14:57:27 +0100

    resizing works

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("label-spacing").addEventListener("change", function() {drawLabels();});
     document.getElementById("label-margin").addEventListener("change", function() {drawLabels();});
     document.getElementById("input_names").addEventListener("oninput", function() {drawLabels();});
-    
+    drawLabels();
     
     
 });
@@ -56,7 +56,7 @@ function drawLabels(){
     let label_spacing = parseFloat(document.getElementById("label-spacing").value);
     let label_margin = parseFloat(document.getElementById("label-margin").value);
 
-    console.log([label_width, label_height,label_margin, label_spacing]);
+    //console.log([label_width, label_height,label_margin, label_spacing]);
 
     let x0 = 0;
     let y0 = 0;
@@ -65,32 +65,42 @@ function drawLabels(){
     let stock_height = document.getElementById("stock-height").value;
     let printed_labels = 0;
     //shape = document.getElementsByTagName("svg")[0];
-    svg.setAttribute("viewBox", "0 0 " + stock_width + " " + stock_height); 
+   // svg.setAttribute("viewBox", "0mm 0mm " + stock_width + " " + stock_height ); 
     svg.appendChild(drawRect(0,0,stock_width,stock_height,"stroke-width:1; fill: none; stroke: #990000"));
     let labelsInRow = Math.floor(stock_width/(label_width+label_spacing));
     
     for(var i = 0; i < lines.length; i++){
         let fontSize = label_height;
-        svg.appendChild(drawRect(x0,y0,label_width,label_height,"stroke-width:1; fill: none; stroke: #009900"));
+        var recty = drawRect(x0,y0,label_width,label_height,"stroke-width:1; fill: none; stroke: #009900");
+        svg.appendChild(recty);
         textline = drawText(x0+label_width/2,y0+label_height/2,lines[i],fontSize);
         svg.appendChild(textline);
         var box = textline.getBBox();
-        // console.log(box.width);
-        // console.log(box.height);
+        let ratioText = textline.getBBox().width / textline.getBBox().height;
+        let ratioRect = recty.getBBox().width / recty.getBBox().height;
+        
+        if(ratioText>ratioRect){
+            console.log("Width is limiting factor");
+            textline.setAttribute("font-size", ((recty.getBBox().width - 2 * label_margin)/textline.getBBox().width) * fontSize);
+        }else{
+            console.log("Height is limiting factor");
+            textline.setAttribute("font-size", ((recty.getBBox().height - 2 * - 2 * label_margin)/textline.getBBox().height) * fontSize);
+        }
+
+        console.log([recty.getBBox().height / recty.getBBox().width, recty.getBBox().height, recty.getBBox().width]);
+        console.log([textline.getBBox().height / textline.getBBox().width, textline.getBBox().height, textline.getBBox().width]);
         
 
-       // textline.setAttribute("x",x0 - box.width/2 + label_width/2);
-       // textline.setAttribute("y",y0 + label_height/2);
-        fontSize = (label_height-label_margin*2)/box.height*label_height;
-        textline.setAttribute("font-size",fontSize);
-        box = textline.getBBox();
-        // console.log(box.width);
-        // console.log(box.height);
+
+        // fontSize = (label_height-label_margin*2)/box.height*label_height;
+        // textline.setAttribute("font-size",fontSize);
+        // box = textline.getBBox();
+
         
-        if(box.width>(label_width-label_margin*2)){
-          fontSize *= label_width / box.width;
-          textline.setAttribute("font-size",fontSize);
-        }
+        // if(box.width>(label_width-label_margin*2)){
+        //   fontSize *= label_width / box.width;
+        //   textline.setAttribute("font-size",fontSize);
+        // }
       
         x0 += label_spacing + label_width;
         if(x0 + label_width>stock_width){
@@ -102,23 +112,32 @@ function drawLabels(){
         }
         //y0 += label_spacing + label_height;  
     }
-
+    resizeSVG();
     downloadSVG();
 
 }
+
+function resizeSVG() {
+    var  svg = document.getElementById("labels");
+    // Get the bounds of the SVG content
+    var  bbox = svg.getBBox();
+    // Update the width and height using the size of the contents
+    svg.setAttribute("viewBox", "0 0 " + bbox.width + " " + bbox.height);
+    //svg.setAttribute("height", bbox.y + bbox.height + bbox.y);
+  }
 function drawRect(x,y,width,height,style){
     let rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-    rect.setAttribute("height",height);
-    rect.setAttribute("width",width);
-    rect.setAttribute("x",x);
-    rect.setAttribute("y",y);
+    rect.setAttribute("height",height+"mm");
+    rect.setAttribute("width",width+"mm");
+    rect.setAttribute("x",x+"mm");
+    rect.setAttribute("y",y+"mm");
     rect.setAttribute("style",style);   
     return rect;
 }
 function drawText(x,y,string,label_height){
     let text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
-    text.setAttribute("x", x);
-    text.setAttribute("y", y);
+    text.setAttribute("x", x+"mm");
+    text.setAttribute("y", y+"mm");
     text.setAttribute("fill", "red");
     text.setAttribute("dominant-baseline", "mathematical");
     text.setAttribute("text-anchor", "middle");
